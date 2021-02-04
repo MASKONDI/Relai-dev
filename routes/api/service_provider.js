0b8db84e88825eca926680e3748538d1dcb743ff
Vivek <vivektripathi10237@gmail.com>
2021-02-04 12:50:33 +0530

    vivek changes

--- routes/api/service_provider.js
+++ routes/api/service_provider.js
@@ -33,22 +33,22 @@ const validateServiceProviderRegisterInput = require('../../Validation/service_p
 const validateServiceProviderSigninInput = require('../../Validation/service_provider_signin');
 
 
-const { service_provider_register, service_provider_signin, service_provider_personal_details, service_provider_other_details, service_provider_Indemnity_details, service_provider_Roles, service_provider_education, service_provider_employment_history, service_provider_language, service_provider_plan, service_provider_portfolio, service_provider_reference, pricing_plan } = require("../../controllers/service_providers");
+////const { service_provider_register, service_provider_signin, service_provider_personal_details, service_provider_other_details, service_provider_Indemnity_details, service_provider_Roles, service_provider_education, service_provider_employment_history, service_provider_language, service_provider_plan, service_provider_portfolio, service_provider_reference, pricing_plan } = require("../../controllers/service_providers");
 
 
 //router.post("/service_provider_register", service_provider_register);
 //router.post("/service_provider_personal_details", service_provider_personal_details);
 //router.post("/service_provider_other_details", service_provider_other_details);
 //router.post("/service_provider_indemnity_details", service_provider_Indemnity_details);
-router.post("/service_provider_roles", service_provider_Roles);
+//router.post("/service_provider_roles", service_provider_Roles);
 //router.post("/service_provider_education", service_provider_education);
 //router.post("/service_provider_employment_history", service_provider_employment_history);
 //router.post("/service_provider_language", service_provider_language);
-router.post("/service_provider_plan", service_provider_plan);
+//router.post("/service_provider_plan", service_provider_plan);
 //router.post("/service_provider_portfolio", service_provider_portfolio);
 //router.post("/service_provider_reference", service_provider_reference);
 //router.post("/service_provider_signin", service_provider_signin);
-router.post("/pricing_plan", pricing_plan);
+//router.post("/pricing_plan", pricing_plan);
 
 
 
@@ -229,13 +229,22 @@ router.post("/service_provider_education", (req, res) => {
   serviceProviderEducation
     .save()
     .then(serviceProviders => {
-      console.log("server response is: ", serviceProviders);
-      res.redirect("/signup-professionals-profile-4")
+      console.log("server response is: ", serviceProviders); 
+      //res.redirect("/signup-professionals-profile-4")
+      res.send({
+         educationDetail:serviceProviders, 
+         status:true
+      });
     })
     .catch(err => {
       console.log(err)
-      req.flash('err_msg', 'Something went wrong please try after some time');
-      res.redirect('/signup-professionals-profile-3');
+      //req.flash('err_msg', 'Something went wrong please try after some time');
+      //res.redirect('/signup-professionals-profile-3');
+      res.send({
+        err_msg:'Something went wrong please try after some time',
+        status:false
+     }); 
+
     });
 });
 
