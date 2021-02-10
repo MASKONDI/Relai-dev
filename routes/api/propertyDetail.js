044222be638277ed152f2641092fda3458c013af
deepaksariya <deepaksariya@questglt.com>
2021-02-10 14:25:05 +0530

    change add-property api

--- routes/api/propertyDetail.js
+++ routes/api/propertyDetail.js
@@ -4,11 +4,11 @@ var fs = require('fs');
 const path = require('path');
 const PropertiesPictureSchema = require("../../models/properties_picture");
 const PropertiesPlanPictureSchema = require("../../models/properties_plan_picture");
-module.exports.GetPropertById = function (propertyId,ps_is_active_user_flag) {
+module.exports.GetPropertById = function (propertyId, ps_is_active_user_flag) {
     return new Promise(async function (resolve, reject) {
         //console.log('hello',pps_property_id,pps_is_active_user_flag)
         //var data = { _id: propertyId }
-        var data={$and:[{ps_is_active_user_flag: ps_is_active_user_flag,_id:propertyId}]}
+        var data = { $and: [{ ps_is_active_user_flag: ps_is_active_user_flag, _id: propertyId }] }
         await PropertiesSchema.findOne(data).then(async (resp) => {
             let responce = await resp
             resolve(responce)
@@ -17,27 +17,163 @@ module.exports.GetPropertById = function (propertyId,ps_is_active_user_flag) {
         })
     });
 };
+module.exports.add_new_property_image = function (req) {
+    return new Promise(async function (resolve, reject) {
+        await req.files.propertiespic.forEach(element => {
+            var obj = {
+                pps_property_id: req.body.property_id,
+                pps_property_image_name: element.filename,
+                pps_is_active_user_flag: req.session.active_user_login,
+                // pps_property_image: {
+                //     data: fs.readFileSync(path.join(__dirname + '../../../public/propimg/' + element.filename)),
+                //     contentType: 'image/png'
+                // }
+            }
+            PropertiesPictureSchema.create(obj, (err, item) => {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    item.save().then(async(propertyImageResponce)=>{
+                        if(propertyImageResponce){
+                            let responce = await propertyImageResponce
+                            resolve(responce);
+                        }
+                    }).catch((err) => {
+                        reject(err)
+                    });
+                }
+            });
+        });
+
+    });
+};
+module.exports.add_new_property_plan_image = function (req) {
+    return new Promise(async function (resolve, reject) {
+        await req.files.planImage.forEach(element => {
+            var obj = {
+                ppps_property_id: req.body.property_id,
+                ppps_plan_image_name: element.filename,
+                ppps_is_active_user_flag: req.session.active_user_login,
+                // ppps_plan_image: {
+                //     data: fs.readFileSync(path.join(__dirname + '../../../public/propplanimg/' + e.filename)),
+                //     contentType: 'image/png'
+                // }
+            }
+            PropertiesPlanPictureSchema.create(obj, (err, item) => {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    item.save().then(async(propertyPlanImageResponce)=>{
+                        if(propertyPlanImageResponce){
+                            let responce = await propertyPlanImageResponce
+                            resolve(responce);
+                        }
+                    }).catch((err) => {
+                        reject(err)
+                    });
+                }
+            });
+        });
+
+    });
+};
+
+module.exports.Add_New_Propert = function (req) {
+    return new Promise(async function (resolve, reject) {
+        var phaseArray = [];
+        for (var i in req.body.Phase) {
+            var phaseObj = {
+                phase_name: '',
+                start_date: '',
+                end_date: '',
+                phase_status: 'pending',
+            };
+            phaseObj.phase_name = req.body.Phase[i];
+            phaseObj.start_date = req.body.startDate[i];
+            phaseObj.end_date = req.body.endDate[i];
+            phaseArray.push(phaseObj);
+        }
+        var PropertyBoject = {
+            ps_unique_code: "prop-" + Math.random().toString(36).slice(-6),
+            ps_user_id: req.session.user_id, //storing customer_ID
+            ps_property_name: req.body.ps_property_name,
+            ps_property_address: req.body.ps_property_address,
+            ps_property_country_id: req.body.country,
+            ps_property_state_id: req.body.state,
+            ps_property_city_id: req.body.city,
+            ps_property_zipcode: req.body.ps_property_zipcode,
+            //ps_property_user_as: req.body.ps_property_user_as,
+            ps_property_user_as: req.session.active_user_login, //updaing active customer portal buyer/seller/renovator
+            ps_other_party_fullname: req.body.ps_other_party_fullname,
+            ps_other_party_emailid: req.body.ps_other_party_emailid,
+            ps_other_party_invited: req.body.ps_other_party_invited,
+            ps_property_area: req.body.ps_property_area,
+            ps_property_bedroom: req.body.ps_property_bedroom,
+            ps_property_bathroom: req.body.ps_property_bathroom,
+            ps_additional_note: req.body.ps_additional_note,
+            ps_property_type: req.body.property_type,
+            ps_is_active_user_flag: req.session.active_user_login,
+            ps_phase_array: phaseArray
+        };
+        const newProperty = new PropertiesSchema(PropertyBoject)
+        await newProperty.save().then(async (property) => {
+
+            if (property) {
+                let responce = await property
+                resolve(responce);
+            }
+
+        }).catch((err) => {
+            reject(err)
+        })
+    });
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports.AddNewProperty = function (req) {
     return new Promise(async function (resolve, reject) {
         //console.log('hello=============', req);
-        
-        var phaseArray=[];
-        for(var i in req.body.Phase){
-            var phaseObj={
-                phase_name:'',
-                start_date:'',
-                end_date:'',
-                phase_status:'pending',
+
+        var phaseArray = [];
+        for (var i in req.body.Phase) {
+            var phaseObj = {
+                phase_name: '',
+                start_date: '',
+                end_date: '',
+                phase_status: 'pending',
             };
-            phaseObj.phase_name=   req.body.Phase[i];
-            phaseObj.start_date=   req.body.startDate[i];
-            phaseObj.end_date=   req.body.endDate[i];
+            phaseObj.phase_name = req.body.Phase[i];
+            phaseObj.start_date = req.body.startDate[i];
+            phaseObj.end_date = req.body.endDate[i];
             phaseArray.push(phaseObj);
-            
-            
-            console.log("phaseObj==========================",phaseObj)
+
+
+            console.log("phaseObj==========================", phaseObj)
         }
-        console.log("phaseArray:============",phaseArray);
+        console.log("phaseArray:============", phaseArray);
 
         var PropertyBoject = {
             ps_unique_code: "prop-" + Math.random().toString(36).slice(-6),
@@ -63,15 +199,15 @@ module.exports.AddNewProperty = function (req) {
             // ps_phase_array:req.body.Phase,
             // ps_phase_start_date:req.body.startDate,
             // pa_phase_end_date:req.body.endDate
-            ps_phase_array:phaseArray
+            ps_phase_array: phaseArray
         };
-        console.log( req.body.Phase.length)
-        
-            
-      
+        console.log(req.body.Phase.length)
 
-           const newProperty =  new PropertiesSchema(PropertyBoject)
-      
+
+
+
+        const newProperty = new PropertiesSchema(PropertyBoject)
+
         //return;
         // var data={_id:propertyId}
         await newProperty.save().then(async (property) => {
@@ -133,7 +269,7 @@ module.exports.AddNewProperty = function (req) {
                 resolve(responce)
             }
             //----------------------------------------
-            
+
         }).catch((err) => {
             reject(err)
         })
