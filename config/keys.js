45542478b146253cd30dfefb5755101f3fb13f01
krishna bihari rajpt <krishnarajput@questglt.com>
2021-02-15 17:25:28 +0530

    krishna changes

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,11 @@
+//Database for testing 
 module.exports = {
   mongoURI: "mongodb+srv://krishna-quest:QuestGLT123@cluster0.yydf9.mongodb.net/test?retryWrites=true&w=majority",
   secretOrKey: "secret"
 };
+
+//Database for developer
+// module.exports = {
+//   mongoURI: "mongodb+srv://Krishna:Krishna123@cluster0.iiahv.mongodb.net/test?retryWrites=true&w=majority",
+//   secretOrKey: "secret"
+// };
