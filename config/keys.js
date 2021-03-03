ec9e8ba548c664fa8a372b7c01de72a98e248cee
Vivek <vivektripathi10237@gmail.com>
2021-03-03 09:56:33 +0530

    vivek keys

--- config/keys.js
+++ config/keys.js
@@ -12,23 +12,23 @@
 //   secretOrKey: "secret"
 // };
 
-module.exports = {
-  mongoURI: "mongodb+srv://krishna-quest:QuestGLT123@cluster0.yydf9.mongodb.net/test?retryWrites=true&w=majority",
-  secretOrKey: "secret",
-  user1: "goLearning006@gmail.com",
-  pass1: "Maskondi#1997",
-  user2: "golearning4@gmail.com",
-  pass2: "Krishna#1997"
-};
- 
-//Database for developer
 // module.exports = {
-//   mongoURI: "mongodb+srv://Krishna:Krishna123@cluster0.iiahv.mongodb.net/test?retryWrites=true&w=majority",
+//   mongoURI: "mongodb+srv://krishna-quest:QuestGLT123@cluster0.yydf9.mongodb.net/test?retryWrites=true&w=majority",
 //   secretOrKey: "secret",
 //   user1: "goLearning006@gmail.com",
 //   pass1: "Maskondi#1997",
 //   user2: "golearning4@gmail.com",
 //   pass2: "Krishna#1997"
 // };
+ 
+//Database for developer
+module.exports = {
+  mongoURI: "mongodb+srv://Krishna:Krishna123@cluster0.iiahv.mongodb.net/test?retryWrites=true&w=majority",
+  secretOrKey: "secret",
+  user1: "goLearning006@gmail.com",
+  pass1: "Maskondi#1997",
+  user2: "golearning4@gmail.com",
+  pass2: "Krishna#1997"
+};
 
 
