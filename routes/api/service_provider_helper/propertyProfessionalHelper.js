a9f12f2a927a653e42be7dd15a8ce339f56edd36
Vivek <vivektripathi10237@gmail.com>
2021-03-11 16:10:30 +0530

    vivek

--- routes/api/service_provider_helper/propertyProfessionalHelper.js
+++ routes/api/service_provider_helper/propertyProfessionalHelper.js
@@ -1,4 +1,6 @@
 const PropertyProfessionalSchema= require('../../../models/property_professional_Schema');
+const ServiceProviderSchema=require("../../../models/service_providers");
+
 module.exports.getHiredPropertyProfessional = function (pps_service_provider_id) {
     return new Promise(async function (resolve, reject) {
         if (pps_service_provider_id != null) {
@@ -16,4 +18,23 @@ module.exports.getHiredPropertyProfessional = function (pps_service_provider_id)
             reject({status:0,'message':'data not found'})
         }
     }).catch((error) => { });
+};
+
+
+
+module.exports.GetProfessionalById = function (ppts_assign_to) {
+    return new Promise( async function (resolve, reject) {
+       // console.log('hello',ppts_assign_to)
+       var data={_id:ppts_assign_to}
+   await ServiceProviderSchema.find(data).then(async(resp)=>{
+       if(resp){
+           //console.log("respArray out of loop",respArray)
+           resolve(resp)
+       }
+       
+       
+    }).catch((err)=>{
+        reject(err)
+    })
+    });
 };
--- routes/spRoutes.js
+++ routes/spRoutes.js
@@ -24,6 +24,9 @@ const CustomerSchema = require("../models/customers");
 //const PropertiesPlanPictureSchema = require("../models/properties_plan_picture");
 const PropertiesSchema = require("../models/properties");
 const PropertyProfessionalSchema = require("../models/property_professional_Schema");
+const CustomerUploadDocsSchema = require("../models/customer_upload_document");
+const TaskHelper = require("./api/addTask");
+
 const ServiceProviderPortfolioSchema = require("../models/service_provider_portfolio");
 const ServiceProviderSchema = require("../models/service_providers");
 const ServiceProviderPersonalDetailsSchema = require("../models/service_provider_personal_details");
@@ -33,6 +36,84 @@ const ServiceProviderLanguageSchema = require("../models/service_provider_langua
 const ServiceProviderEmploymentHistorySchema = require('../models/service_provider_employment_history');
 const ServiceProviderReferenceSchema = require("../models/service_provider_reference");
 const ServiceProviderIndemnityDetailsSchema = require("../models/service_provider_indemnity_details");
+
+const MessageSchema = require("../models/message");
+
+function tallyVotes(AllhiredProfeshnoal) {
+  return AllhiredProfeshnoal.reduce((total, i) => total + i.pps_pofessional_budget, 0);
+}
+
+function timeDifference(data) {
+  var date = [];
+  data.forEach(function (item) {
+    var startDate = "";
+    var endDate = "";
+    let diffInMilliSeconds;
+    var days = "";
+    console.log("start date" + item.start_date + " end Date" + item.end_date);
+    // message = message + calcDate(item.start_date, item.end_date);
+    startDate = new Date(Date.parse(item.start_date));
+    endDate = new Date(Date.parse(item.end_date));
+    diffInMilliSeconds = Math.abs(endDate - startDate) / 1000;
+    // calculate days
+    days = Math.floor(diffInMilliSeconds / 86400);
+    date.push(days);
+
+    console.log("total date is :", days);
+
+  });
+  var estimated_time = date.reduce((total, i) => total + i, 0);
+  var message = "";
+  if (estimated_time > 365) {
+    var year = Math.floor(estimated_time / 365);
+    var days = estimated_time % 365;
+    if (days > 31) {
+      var months = Math.floor(days / 31);
+      var day = days % 31;
+      message += year + " year " + months + " months " + day + " days"
+    }
+  }
+  else if (estimated_time > 31) {
+    var months = Math.floor(estimated_time / 31);
+    var day = estimated_time % 31;
+    message += months + " months " + day + " days"
+  } else {
+    message += estimated_time + " days "
+  }
+  console.log("estimated_time :", message);
+  if (isNaN(estimated_time)) {
+    console.log("aaa", estimated_time)
+    return 'N/A';
+  } else {
+    console.log("bbb", estimated_time)
+    return message;
+  }
+}
+
+function getPhase(phase) {
+  if (phase == 'A') {
+    return 'mydreamhome-details-phase-a';
+  } else if (phase == 'B') {
+    return 'mydreamhome-details-phase-b';
+  } else if (phase == 'C') {
+    return 'mydreamhome-details-phase-c';
+  } else if (phase == 'D') {
+    return 'mydreamhome-details-phase-d';
+  } else if (phase == 'E') {
+    return 'mydreamhome-details-phase-e';
+  } else if (phase == 'F') {
+    return 'mydreamhome-details-phase-f';
+  } else if (phase == 'G') {
+    return 'mydreamhome-details-phase-g';
+  } else if (phase == 'H') {
+    return 'mydreamhome-details-phase-h';
+  } else {
+    return 'mydreamhome-details-phase-o';
+  }
+
+
+}
+
 app.get('/signup-professionals-profile', isServiceProvider, (req, res) => {
   err_msg = req.flash('err_msg');
   success_msg = req.flash('success_msg');
@@ -257,17 +338,7 @@ app.get('/service-provider/myproperties', isServiceProvider, async function (req
   })
 });
 
-app.get('/service-provider/myproperties-detail', isServiceProvider, async function (req, res) {
-  console.log("", req.session);
-  //console.log('data===========',data)
-  req.session.pagename = 'service-provider/property';
-  err_msg = req.flash('err_msg');
-  success_msg = req.flash('success_msg');
-  res.render('service-provider/myproperties-detail', {
-    err_msg, success_msg, layout: false,
-    session: req.session
-  });
-});
+
 
 app.get('/service-provider/property-detail', isServiceProvider, function (req, res) {
 
@@ -591,6 +662,174 @@ app.get('/getLanguagedetails', isServiceProvider, async function (req, res) {
   })
 });
 
+
+
+app.get('/service-provider/myproperties-detail', isServiceProvider, async function (req, res) {
+  console.log("", req.session);
+  req.session.pagename = 'service-provider/property';
+  if (req.query.id) {
+    req.session.property_id = req.query.id
+    console.log("req.query.id:", req.query.id);
+    PropertiesSchema.find({ _id: req.query.id}).then(async (data) => {
+      if (data) {
+
+        
+        var EstimatedTime = timeDifference(data[0].ps_phase_array);
+        let arr = [];
+        for (let img of data) {
+          await PropertiesPictureSchema.find({ pps_property_id: img._id }).then(async (result) => {
+            //let temp = await result
+            for (let image of result) {
+              let temp = await image
+              arr.push(temp)
+            }
+          })
+        }
+
+        let allDocumentUploadByCustmer = await CustomerUploadDocsSchema.find({cuds_property_id: req.query.id});
+        //console.log('AllhiredProfeshnoal', AllhiredProfeshnoal);
+        let serviceProvArray = [];
+        let totalcostArray = [];
+        var cost = 0;
+
+        let CustomerDetails = await CustomerSchema.find({ _id: data[0].ps_user_id });
+        if(CustomerDetails){
+            //serviceProvArray.push(CustomerDetails)        
+          for (let customerDetail of CustomerDetails) {
+            await MessageSchema.findOne({
+              $or: [
+                { $and: [{ sms_sender_id: req.session.user_id }, { sms_receiver_id: customerDetail.id }, { sms_property_id: req.session.property_id }] },
+                { $and: [{ sms_sender_id: customerDetail.id }, { sms_receiver_id: req.session.user_id }, { sms_property_id: req.session.property_id }] }
+              ]
+            }).sort({ _id: -1 }).then(async (msgdata) => {
+              console.log('msgdata=', msgdata)
+                if(msgdata !== null){
+                      //for (let i of allProfeshnoals) {
+                            if (msgdata) {
+                              var object_as_string = JSON.stringify(customerDetail);
+                              const t = JSON.parse(object_as_string);
+                              // console.log('lastIndex:', msgdata.slice(-1)[0]);
+                              let msgData = msgdata;
+                              t.sms_message = msgData.sms_message;
+                              //let temps = await i
+                              serviceProvArray.push(t)
+                            } else {
+                              var object_as_string = JSON.stringify(customerDetail);
+                              const t = JSON.parse(object_as_string);
+                              t.sms_message = '...';
+                              //let temps = await i
+                              serviceProvArray.push(t)
+                            }
+                      //}
+                }else{
+                      console.log('myfff:')
+                }
+            })
+        }
+      }
+
+      console.log('serviceProvArraymyfff:',serviceProvArray);
+
+
+        // var sumof = tallyVotes(to)
+        // var sumof = tallyVotes(AllhiredProfeshnoal)
+        // console.log("sumofsumofsumofsumofsumof", sumof)
+        // for (var k of AllhiredProfeshnoal) {
+        //   var costs = parseInt(k.pps_pofessional_budget);
+        //   cost = await parseInt(cost + costs);
+        //   totalcostArray.push(cost)
+
+        //   await ServiceProviderSchema.find({ _id: k.pps_service_provider_id }).then(async (allProfeshnoals) => {
+        //     //console.log('allProfeshnoals:', allProfeshnoals)
+        //     await MessageSchema.find({
+        //       $or: [
+        //         { $and: [{ sms_sender_id: req.session.user_id }, { sms_receiver_id: k.pps_service_provider_id }, { sms_is_active_user_flag: req.session.active_user_login }, { sms_property_id: req.session.property_id }] },
+        //         { $and: [{ sms_sender_id: k.pps_service_provider_id }, { sms_receiver_id: req.session.user_id }, { sms_property_id: req.session.property_id }] }
+        //       ]
+        //     }).then(async (msgdata) => {
+        //       //console.log('msgdata=', msgdata)
+        //       for (let i of allProfeshnoals) {
+        //         if (msgdata.length > 0) {
+        //           var object_as_string = JSON.stringify(i);
+        //           const t = JSON.parse(object_as_string);
+        //           // console.log('lastIndex:', msgdata.slice(-1)[0]);
+        //           let msgData = msgdata.slice(-1)[0];
+        //           t.sms_message = msgData.sms_message;
+        //           //let temps = await i
+        //           serviceProvArray.push(t)
+        //         } else {
+        //           var object_as_string = JSON.stringify(i);
+        //           const t = JSON.parse(object_as_string);
+        //           t.sms_message = '...';
+        //           //let temps = await i
+        //           serviceProvArray.push(t)
+        //         }
+        //       }
+        //     })
+        //   });
+        // }
+
+
+
+        let todoArray = [];
+        var c = 0;
+        let TaskDetailObj = await TaskHelper.GetTaskById(req.query.id, req.session.active_user_login);
+        var phase_page_name = '';
+        for (var ph of TaskDetailObj) {
+          var ServiceProffId='';
+          const PhaseObject = JSON.stringify(ph);
+          const to_do_data = JSON.parse(PhaseObject);
+          phase_page_name = await getPhase(to_do_data.ppts_phase_flag);
+          to_do_data.phase_page_name = phase_page_name
+          if (typeof (to_do_data.ppts_assign_to) == 'object') { 
+            var serviceId = to_do_data.ppts_assign_to.includes(req.session.user_id);
+            if(serviceId == true)
+            {
+              ServiceProffId = req.session.user_id;
+
+              let professionalObj = await propertyProfessinoal.GetProfessionalById(ServiceProffId);
+              if (professionalObj) {
+                for (var Prof_fullname of professionalObj) {
+                  to_do_data.professionalName = Prof_fullname.sps_fullname
+                  todoArray.push(to_do_data);
+                }
+              }
+
+            }
+          }
+        }
+        err_msg = req.flash('err_msg');
+        success_msg = req.flash('success_msg');
+        res.render('service-provider/myproperties-detail', {
+          err_msg, success_msg, layout: false,
+          session: req.session,
+          propertyDetailData: data,
+          propertyImage: arr,
+          hiredProfeshnoalList: serviceProvArray,
+          allDocumentUploadByCustmer: allDocumentUploadByCustmer,
+          TaskDetailObj: todoArray,
+          CustomerDetails:CustomerDetails,
+         // totalcost: sumof,
+          estimated_time: EstimatedTime,
+          moment: moment
+
+        });
+      }
+    }).catch((err) => {
+      console.log(err)
+    })
+  } else {
+    res.redirect('/myproperties');
+  }
+
+
+  // err_msg = req.flash('err_msg');
+  // success_msg = req.flash('success_msg');
+  // res.render('service-provider/myproperties-detail', {
+  //   err_msg, success_msg, layout: false,
+  //   session: req.session
+  // });
+});
 
 module.exports = app;
 
